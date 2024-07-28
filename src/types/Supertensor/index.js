b48978c3676b981009306e1c149c363a9d8bb370
exactchange <benny@exactchange.network>
2024-07-27 22:14:46 -0700

    simplify constructor

--- src/types/Supertensor/index.js
+++ src/types/Supertensor/index.js
@@ -6,15 +6,9 @@
 
 import Tensor from '../Tensor';
 
-const TYPE_ERROR = 'TypeError: Invalid tensor data.';
-
 class Supertensor extends Array {
   constructor (tensorArray) {
-    if (!tensorArray.every(tensor => tensor instanceof Tensor)) {
-      throw TYPE_ERROR;
-    }
-
-    super(...tensorArray);
+    super(...tensorArray.map(tensor => new Tensor(tensor)));
   }
 }
 
